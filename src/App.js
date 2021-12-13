744992331a8634720a027d7e064fd16dd66356f1
Alexander Ames <alexanderthomasames@gmail.com>
2021-12-13 13:59:52 -0700

    * assign query and print results

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
 import "./App.css";
-import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+import { gql, useQuery, useMutation } from "@apollo/client";
 
-const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
+const GET_ITEMS = gql`
+  query items {
+    items {
+      id
+      name
     }
   }
 `;
 
 function App() {
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  const [userInput, setUserInput] = useState("");
+  const { loading, error, data } = useQuery(GET_ITEMS);
+  // const [updateItems, updateItemsStatus] = useMutation(GET_ITEMS);
+  // TODO: update to debounce
+  const onUserInput = ({ target }) => {
+    console.log(target.value);
+    setUserInput(target.value);
+  };
+
+  const fireUserInput = () => {
+    console.log(userInput);
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error..</p>;
 
   return (
-    <>
-    <input type="text" />
-    <button>Submit</button>
-      {data.rates.map(({ currency, rate }) => (
-        <div key={currency}>
-          <p>
-            {currency}: {rate}
-          </p>
+    <div className="container">
+      <div className="row py-5">
+        <div className="col-6 mx-auto w-25">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Recipient's username"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+              onChange={onUserInput}
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
+              onClick={fireUserInput}
+            >
+              submit
+            </button>
+          </div>
         </div>
-      ))}
-    </>
+        <div className="col-6">
+          <ul className="list-group list-group-flush">
+            {data.items.map(({ id, name }) => (
+              <li className="list-group-item" key={id}>
+                {name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
