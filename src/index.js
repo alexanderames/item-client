2cb1ef3e3a7daa85ae81f82617e4afa0d31e1868
Alexander Ames <alexanderthomasames@gmail.com>
2021-12-13 13:59:29 -0700

    * assign uri

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache(),
 });
 
